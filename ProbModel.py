7af859dacdb2654acb79d3245b23db901a305989
m.alehdaghi <mahdi.alehdaghi@gmail.com>
2017-11-18 20:48:10 +0330

    (wip) Tries use matrix operators

--- ProbModel.py
+++ ProbModel.py
@@ -8,6 +8,7 @@ class ProbModel:
         self.BLOCK_SIZE	= 4
         self.INIT_BG_VAR = 25 * 25
         self.VAR_THRESH_MODEL_MATCH = 2
+        self.VAR_THRESH_FG_DETERMINE = 4.0
         self.curImage = None
         self.means = None
         self.vars = None
@@ -15,6 +16,7 @@ class ProbModel:
         self.temp_means = None
         self.temp_vars = None
         self.temp_ages = None
+        self.distImg = None
 
         self.modelIndexes = None
         self.modelWidth = None
@@ -29,10 +31,16 @@ class ProbModel:
         (self.modelHeight, self.modelWidth) = (self.obsHeight/self.BLOCK_SIZE, self.obsWidth/self.BLOCK_SIZE)
         self.means = np.zeros((self.NUM_MODELS, self.modelHeight, self.modelWidth))
         self.vars = np.zeros((self.NUM_MODELS, self.modelHeight, self.modelWidth))
-        self.ages = np.zeros((self.NUM_MODELS, self.modelHeight, self.modelWidth))
+        self.ages = np.arange(self.NUM_MODELS * self.modelHeight * self.modelWidth)
+        self.ages = (self.ages * self.ages + self.ages) % 123
+
+        self.ages = self.ages.reshape(self.NUM_MODELS, self.modelHeight, self.modelWidth)
+
         self.modelIndexes = np.zeros((self.modelWidth, self.modelHeight))
         self.means = np.arange(self.NUM_MODELS * self.modelHeight * self.modelWidth).reshape(self.NUM_MODELS, self.modelHeight, self.modelWidth).astype(float)
         self.temp_means = -1 * np.ones((self.NUM_MODELS, self.modelHeight, self.modelWidth))
+        self.temp_ages = -1 * np.ones((self.NUM_MODELS, self.modelHeight, self.modelWidth))
+        self.temp_vars = -1 * np.ones((self.NUM_MODELS, self.modelHeight, self.modelWidth))
         h = np.identity(3)
 
     def shift(self, arr, rr, cc):
@@ -44,9 +52,18 @@ class ProbModel:
         return np.pad(arr, (t1, t2), 'constant')[a:b, c:d]
 
     def rebin(self, arr, factor):
-        temp = np.pad(arr, ((0,arr.shape[0] % factor[0]), (0, arr.shape[1] % factor[1])), 'edge')
+        f = (np.asarray(factor) - arr.shape) % factor
+        temp = np.pad(arr, ((0, f[0]), (0, f[1])), 'edge')
         sh = temp.shape[0] // factor[0], factor[0], -1, factor[1]
-        return temp.reshape(sh).mean(-1).mean(1)
+        res = temp.reshape(sh).mean(-1).mean(1)
+        return res[:res.shape[0] - f[0], : res.shape[1] - f[1]]
+
+    def rebinMax(self, arr, factor):
+        f = (np.asarray(factor) - arr.shape) % factor
+        temp = np.pad(arr, ((0, f[0]), (0, f[1])), 'edge')
+        sh = temp.shape[0] // factor[0], factor[0], -1, factor[1]
+        res = temp.reshape(sh).max(-1).max(1)
+        return res[:res.shape[0] - f[0], : res.shape[1] - f[1]]
 
     def motionCompensate(self, H):
 
@@ -73,82 +90,87 @@ class ProbModel:
         aDi = abs(Di)
         aDj = abs(Dj)
 
-        M = self.means[0]
-        V = self.vars[0]
-        A = self.ages[0]
+        M = self.means
+        V = self.vars
+        A = self.ages
 
         W_H = (aDi * (1 - aDj)).reshape(self.modelHeight, self.modelWidth)
         W_V = (aDj * (1 - aDi)).reshape(self.modelHeight, self.modelWidth)
         W_HV = (aDi * aDj).reshape(self.modelHeight, self.modelWidth)
         W_self = ((1-aDi) * (1 - aDj)).reshape(self.modelHeight, self.modelWidth)
 
-        W = np.zeros((self.modelHeight, self.modelWidth))
+        W = np.zeros((self.NUM_MODELS, self.modelHeight, self.modelWidth))
 
-        tempMean = np.zeros(self.means[0].shape)
-        tempAges = np.zeros(self.means[0].shape)
+        tempMean = np.zeros(self.means.shape)
+        tempAges = np.zeros(self.means.shape)
 
 
         NewI_H = idxNewI + np.sign(Di).astype(int)
         condH = (idxNewJ >= 0) & (idxNewJ < self.modelHeight) & (NewI_H >= 0) & (NewI_H < self.modelWidth)
-        tempMean[J[condH], I[condH]] = W_H[J[condH], I[condH]] * M[idxNewJ[condH], NewI_H[condH]]
-        tempAges[J[condH], I[condH]] = W_H[J[condH], I[condH]] * A[idxNewJ[condH], NewI_H[condH]]
-        W[J[condH], I[condH]] += W_H[J[condH], I[condH]]
+
+        tempMean[:, J[condH], I[condH]] = W_H[J[condH], I[condH]] * M[:, idxNewJ[condH], NewI_H[condH]]
+        tempAges[:, J[condH], I[condH]] = W_H[J[condH], I[condH]] * A[:, idxNewJ[condH], NewI_H[condH]]
+        W[:, J[condH], I[condH]] += W_H[J[condH], I[condH]]
 
         NewJ_V = idxNewJ + np.sign(Dj).astype(int)
         condV = (NewJ_V >= 0) & (NewJ_V < self.modelHeight) & (idxNewI >= 0) & (idxNewI < self.modelWidth)
-        tempMean[J[condV], I[condV]] += W_V[J[condV], I[condV]] * M[NewJ_V[condV], idxNewI[condV]]
-        tempAges[J[condV], I[condV]] += W_V[J[condV], I[condV]] * A[NewJ_V[condV], idxNewI[condV]]
-        W[J[condV], I[condV]] += W_V[J[condV], I[condV]]
+        tempMean[:, J[condV], I[condV]] += W_V[J[condV], I[condV]] * M[:, NewJ_V[condV], idxNewI[condV]]
+        tempAges[:, J[condV], I[condV]] += W_V[J[condV], I[condV]] * A[:, NewJ_V[condV], idxNewI[condV]]
+        W[:, J[condV], I[condV]] += W_V[J[condV], I[condV]]
 
         NewI_H = idxNewI + np.sign(Di).astype(int)
         NewJ_V = idxNewJ + np.sign(Dj).astype(int)
         condHV = (NewJ_V >= 0) & (NewJ_V < self.modelHeight) & (NewI_H >= 0) & (NewI_H < self.modelWidth)
-        tempMean[J[condHV], I[condHV]] += W_HV[J[condHV], I[condHV]] * M[NewJ_V[condHV], NewI_H[condHV]]
-        tempAges[J[condHV], I[condHV]] += W_HV[J[condHV], I[condHV]] * A[NewJ_V[condHV], NewI_H[condHV]]
-        W[J[condHV], I[condHV]] += W_HV[J[condHV], I[condHV]]
+        tempMean[:, J[condHV], I[condHV]] += W_HV[J[condHV], I[condHV]] * M[:, NewJ_V[condHV], NewI_H[condHV]]
+        tempAges[:, J[condHV], I[condHV]] += W_HV[J[condHV], I[condHV]] * A[:, NewJ_V[condHV], NewI_H[condHV]]
+        W[:, J[condHV], I[condHV]] += W_HV[J[condHV], I[condHV]]
 
         condSelf = (idxNewJ >= 0) & (idxNewJ < self.modelHeight) & (idxNewI >= 0) & (idxNewI < self.modelWidth)
-        tempMean[J[condSelf], I[condSelf]] += W_self[J[condSelf], I[condSelf]] * M[idxNewJ[condSelf], idxNewI[condSelf]]
-        tempAges[J[condSelf], I[condSelf]] += W_self[J[condSelf], I[condSelf]] * A[idxNewJ[condSelf], idxNewI[condSelf]]
-        W[J[condSelf], I[condSelf]] += W_self[J[condSelf], I[condSelf]]
+        tempMean[:, J[condSelf], I[condSelf]] += W_self[J[condSelf], I[condSelf]] * M[:, idxNewJ[condSelf], idxNewI[condSelf]]
+        tempAges[:, J[condSelf], I[condSelf]] += W_self[J[condSelf], I[condSelf]] * A[:, idxNewJ[condSelf], idxNewI[condSelf]]
+        W[:, J[condSelf], I[condSelf]] += W_self[J[condSelf], I[condSelf]]
 
-        self.temp_means[0][W != 0] = 0
+        self.temp_means[W != 0] = 0
+        self.temp_ages = 0
         W[W == 0] = 1
-        self.temp_means[0] += tempMean / W
-        self.temp_means[1] += tempAges / W
+        self.temp_means += tempMean / W
+        self.temp_ages += tempAges / W
 
-        temp_var = np.zeros(self.means[0].shape)
+        temp_var = np.zeros(self.means.shape)
 
-        temp_var[J[condH], I[condH]] += W_H[J[condH], I[condH]] * (V[idxNewJ[condH], NewI_H[condH]] +
-                                                                  np.power(self.temp_means[0][J[condH], I[condH]] -
-                                                                           self.means[0][idxNewJ[condH], NewI_H[condH]],
+        temp_var[:, J[condH], I[condH]] += W_H[J[condH], I[condH]] * (V[:, idxNewJ[condH], NewI_H[condH]] +
+                                                                  np.power(self.temp_means[:, J[condH], I[condH]] -
+                                                                           self.means[:, idxNewJ[condH], NewI_H[condH]],
                                                                            2))
 
-        temp_var[J[condV], I[condV]] += W_V[J[condV], I[condV]] * (V[NewJ_V[condV], idxNewI[condV]] +
-                                                                   np.power(self.temp_means[0][J[condV], I[condV]] -
-                                                                            self.means[0][
+        temp_var[:, J[condV], I[condV]] += W_V[J[condV], I[condV]] * (V[:, NewJ_V[condV], idxNewI[condV]] +
+                                                                   np.power(self.temp_means[:, J[condV], I[condV]] -
+                                                                            self.means[:,
                                                                                 NewJ_V[condV], idxNewI[condV]],
                                                                             2))
 
-        temp_var[J[condHV], I[condHV]] += W_HV[J[condHV], I[condHV]] * (V[NewJ_V[condHV], NewI_H[condHV]] +
-                                                                  np.power(self.temp_means[0][J[condHV], I[condHV]] -
-                                                                           self.means[0][NewJ_V[condHV], NewI_H[condHV]],
+        temp_var[:, J[condHV], I[condHV]] += W_HV[J[condHV], I[condHV]] * (V[:, NewJ_V[condHV], NewI_H[condHV]] +
+                                                                  np.power(self.temp_means[:, J[condHV], I[condHV]] -
+                                                                           self.means[:, NewJ_V[condHV], NewI_H[condHV]],
                                                                            2))
 
-        temp_var[J[condSelf], I[condSelf]] += W_self[J[condSelf], I[condSelf]] * (V[idxNewJ[condSelf], idxNewI[condSelf]] +
-                                                                  np.power(self.temp_means[0][J[condSelf], I[condSelf]] -
-                                                                           self.means[0][idxNewJ[condSelf], idxNewI[condSelf]],
+        temp_var[:, J[condSelf], I[condSelf]] += W_self[J[condSelf], I[condSelf]] * (V[:, idxNewJ[condSelf], idxNewI[condSelf]] +
+                                                                  np.power(self.temp_means[:, J[condSelf], I[condSelf]] -
+                                                                           self.means[:, idxNewJ[condSelf], idxNewI[condSelf]],
                                                                            2))
+        self.temp_vars += temp_var / W
 
-        print (temp_var/W).reshape(self.modelHeight* self.modelWidth)
 
 
     def update(self):
+        print self.temp_ages[0].reshape(-1)
+        print self.temp_ages[1].reshape(-1)
+
         curMean = self.rebin(self.curImage, (self.BLOCK_SIZE, self.BLOCK_SIZE))
         mm = np.argmax(self.temp_ages, axis=0).reshape(-1)
         maxes = np.max(self.temp_ages, axis=0)
-        h, w = self.modelWidth * self.modelHeight
-        jj, ii = np.arange(h*w)/w, np.arange(h*w)%w
+        h, w = self.modelWidth , self.modelHeight
+        jj, ii = np.arange(h*w)/h, np.arange(h*w)%h
         ii, jj = ii[mm != 0], jj[mm != 0]
         mm = mm[mm != 0]
         self.temp_ages[mm, jj, ii] = 0
@@ -160,12 +182,37 @@ class ProbModel:
         self.temp_vars[0, jj, ii] = self.temp_vars[mm, jj, ii]
         self.temp_vars[mm, jj, ii] = self.INIT_BG_VAR
 
-        modelIndex = np.ones(curMean.shape)
+        modelIndex = np.ones(curMean.shape).astype(int)
         cond1 = np.power(curMean - self.temp_means[0], 2) < self.VAR_THRESH_MODEL_MATCH * self.temp_vars[0]
         modelIndex[cond1] = 0
         cond2 = np.power(curMean - self.temp_means[1], 2) < self.VAR_THRESH_MODEL_MATCH * self.temp_vars[1]
         modelIndex[cond2] = 1
-        self.temp_ages[1][(~cond1) & ~(cond2)] = 0
+        self.temp_ages[1][(~cond1) & (~cond2)] = 0
+
+        print self.temp_ages[0].reshape(-1)
+        print self.temp_ages[1].reshape(-1)
+
+
+        alpha = self.temp_ages / (self.temp_ages + 1)
+        alpha[self.temp_ages < 1] = 0
+        self.means = self.temp_means * (1 - alpha)
+        jj, ii = np.arange(h*w) / h, np.arange(h*w) % h
+        self.means[modelIndex.reshape(-1), jj, ii] += curMean[jj, ii] * alpha[modelIndex.reshape(-1), jj, ii]
+
+        bigMeanIndex = np.kron(self.means[modelIndex.reshape(-1), jj, ii].reshape(w, -1), np.ones((self.BLOCK_SIZE, self.BLOCK_SIZE)))
+        bigMean = np.kron(self.means[0], np.ones((self.BLOCK_SIZE, self.BLOCK_SIZE)))
+        bigAges = np.kron(self.ages[0], np.ones((self.BLOCK_SIZE, self.BLOCK_SIZE)))
+        bigVars = np.kron(self.vars[0], np.ones((self.BLOCK_SIZE, self.BLOCK_SIZE)))
+        (a, b) = (self.curImage.shape[0] - bigMean.shape[0], self.curImage.shape[1] - bigMean.shape[1])
+        bigMean = np.pad(bigMean, ((0, a), (0, b)), 'edge')
+        bigAges = np.pad(bigAges, ((0, a), (0, b)), 'edge')
+        bigVars = np.pad(bigVars, ((0, a), (0, b)), 'edge')
+        bigMeanIndex = np.pad(bigMeanIndex, ((0, a), (0, b)), 'edge')
+
+        temp = self.rebinMax(np.powewr(self.curImage - bigMeanIndex, 2), (self.BLOCK_SIZE, self.BLOCK_SIZE))
+        self.distImg = np.power(self.curImage - bigMean, 2)
+        out = np.zeros(self.curImage.shape)
+        out[(bigAges > 1) & (self.distImg > self.VAR_THRESH_FG_DETERMINE * bigVars)] = 255
 
 
 
--- main.py
+++ main.py
@@ -20,9 +20,10 @@ import ProbModel
 # cv2.destroyAllWindows()
 gray1 = cv2.imread('/home/mahdi/Apps/others/probModel/gg1.jpg', 2)
 gray2 = cv2.imread('/home/mahdi/Apps/others/probModel/gg2.jpg', 2)
+np.set_printoptions(precision=2)
 model = ProbModel.ProbModel()
 model.init(gray1)
 h = np.asarray([0.19658485, -0.14968985, -0.00128087, 0.45442999, 0.18668709, 0.07489325, 0.07402678, -0.48996603, 0.3031477])\
     .reshape(3,3)
 model.motionCompensate(h)
-#model.update(gray2)
+model.update()
